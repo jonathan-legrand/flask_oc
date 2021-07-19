cb5bdbe3c8f05582ed30013b9455a7b188d64a8a
jonathan-sudo <johnlegrand972@gmail.com>
2021-07-19 16:41:37 +0200

    Upgrading gender attribute

--- fbapp/models.py
+++ fbapp/models.py
@@ -1,19 +1,33 @@
 from flask_sqlalchemy import SQLAlchemy
 
 from .views import app
+import logging as lg
+import enum
 
 db = SQLAlchemy(app)
+
+
+class Gender(enum.Enum):
+    female = 0
+    male = 1
+    other = 2
 
 
 class Content(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(200), nullable=False)
-    gender = db.Column(db.Integer(), nullable=False)
+    gender = db.Column(db.Enum(Gender), nullable=False)
 
     def __init__(self, description, gender):
         self.description = description
         self.gender = gender
 
 
-db.create_all()
+def init_db():
+    db.drop_all()
+    db.create_all()
+    db.session.add(Content("THIS IS SPARTAAAAAAA!!!", Gender['male']))
+    db.session.add(Content("What's your favorite scary movie?", Gender['other']))
+    db.session.commit()
+    lg.warning('Database initialized!')
