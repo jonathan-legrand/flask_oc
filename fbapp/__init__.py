d6a0e898f1675fe967172594aa214b601594c1ea
jonathan-sudo <johnlegrand972@gmail.com>
2021-07-19 16:40:54 +0200

    DB initialisation

--- fbapp/__init__.py
+++ fbapp/__init__.py
@@ -1,7 +1,17 @@
 from flask import Flask
-from .views import app
 
+from .views import app
 from . import models
 
 # Connect sqlalchemy to app
 models.db.init_app(app)
+
+
+@app.cli.command("init_db")
+def init_db():
+    models.init_db()
+
+
+@app.cli.command("say_hello")
+def say_hello():
+    print("hello")
