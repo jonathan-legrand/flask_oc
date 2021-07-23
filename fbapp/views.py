106fa5b504e87086b389f943c58ee2bb14ca73e8
jonathan-sudo <johnlegrand972@gmail.com>
2021-07-24 00:35:58 +0200

    Mtnt çava

--- fbapp/views.py
+++ fbapp/views.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, url_for, request
-from logging import warning
 
 app = Flask(__name__)
 
@@ -13,59 +12,45 @@ from .utils import find_content, OpenGraphImage
 @app.route('/')
 @app.route('/index/')
 def index():
-    description = "Toi, tu sais comment utiliser la console ! \
-    Jamais à court d'idées pour réaliser ton objectif, tu es déterminé-e et persévérant-e. \
-    Tes amis disent d'ailleurs volontiers que tu as du caractère et que tu ne te laisses pas marcher sur les pieds. \
-    Un peu hacker sur les bords, tu aimes trouver des solutions à tout problème. \
-    N'aurais-tu pas un petit problème d'autorité ? ;-)"
-    page_title = request.args.get("page")
-    img = request.args.get("img")
-    if img is None:
-        warning("No image has been specified")
-        img = "tmp/cover_111823112767411.jpg"
-    og_url = url_for("static", filename=img, _external=True)
-    print(img)
-    return render_template("index.html",
-                           user_name="JB",
+    if 'img' in request.args:
+        img = request.args['img']
+        og_url = url_for('index', img=img, _external=True)
+        og_image = url_for('static', filename=img, _external=True)
+    else:
+        og_url = url_for('index', _external=True)
+        og_image = url_for('static', filename='tmp/sample.jpg', _external=True)
+    description = """
+        Toi, tu sais comment utiliser la console ! Jamais à court d'idées pour réaliser ton objectif, tu es déterminé-e et persévérant-e. Tes amis disent d'ailleurs volontiers que tu as du caractère et que tu ne te laisses pas marcher sur les pieds. Un peu hacker sur les bords, tu aimes trouver des solutions à tout problème. N'aurais-tu pas un petit problème d'autorité ? ;-)
+    """
+    page_title = "Le test ultime"
+
+    og_description = "Découvre qui tu es vraiment en faisant le test ultime !"
+    return render_template('index.html',
+                           user_name='Julio',
                            user_image=url_for('static', filename='img/profile.png'),
                            description=description,
                            blur=True,
                            page_title=page_title,
                            og_url=og_url,
-                           og_img=img
-                           )
+                           og_image=og_image,
+                           og_description=og_description)
 
 
 @app.route('/result/')
 def result():
-    gender = request.args.get("gender")
-
-    try:
-        description = find_content(gender).description
-    except IndexError:
-        warning("Gender {} is not present in the database".format(gender))
-        description = "Tu es unique"
-
-    # print(description)
-    user_name = request.args.get("first_name")
-    uid = request.args.get("id")
-    og_img = OpenGraphImage(uid, user_name, description).location
-    og_url = url_for("static", filename=og_img, _external=True)
+    gender = request.args.get('gender')
+    user_name = request.args.get('first_name')
+    uid = request.args.get('id')
     profile_pic = 'http://graph.facebook.com/' + uid + '/picture?type=large'
-    return render_template("result.html",
+    description = find_content(gender).description
+    img = OpenGraphImage(uid, user_name, description).location
+    og_url = url_for('index', img=img, _external=True)
+    return render_template('result.html',
                            user_name=user_name,
-                           gender=gender,
-                           description=description,
-                           uid=uid,
                            user_image=profile_pic,
-                           og_url=og_url,
-                           blur=False)
+                           description=description,
+                           og_url=og_url)
 
-
-@app.route('/contents/<int:content_id>/')
-def content(content_id):
-    return "content_id : " + str(content_id)
-
-
-if __name__ == "__main__":
-    app.run()
+# @app.route('/contents/<int:content_id>/')
+# def content(content_id):
+#     return '%s' % content_id
